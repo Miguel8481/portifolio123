61d1dbd9fae55be21c52e17edac01492d7e7f0f1
luiz8481 <luiz848115@gmail.com>
2025-05-22 23:01:42 -0300

    firebase

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB1XUtTw4VIBIor-948Y_MW1TfIYugwDnQ",
+  authDomain: "portfolio-cda39.firebaseapp.com",
+  databaseURL: "https://portfolio-cda39-default-rtdb.firebaseio.com",
+  projectId: "portfolio-cda39",
+  storageBucket: "portfolio-cda39.firebasestorage.app",
+  messagingSenderId: "545004645429",
+  appId: "1:545004645429:web:3944778142fea65d18d57c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
